fabe1c213443a97742aa6c4e3f24d7a2c27a120c
Nayyab Akbar <nayyabakbar10@gmail.com>
2024-02-08 16:03:38 +0500

    updated notifications

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,6 +1,7 @@
 const { initializeApp, applicationDefault } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 const User = require("../src/models/userSchema");
+const Notification = require("../src/models/notificationSchema");
 
 initializeApp({
   credential: applicationDefault(),
@@ -9,20 +10,25 @@ initializeApp({
 
 async function send(req, res) {
   try {
-    const user = await User.findById(req.body.userId);
-    const fcmToken = user.fcmToken;
-    const username = req.body.name;
-
+    const receivingUser = await User.findById(req.body.userId);
+    const senderUser = await User.findById(req.user.user);
+    const fcmToken = receivingUser.fcmToken;
+    const username = senderUser.name;
+    const notificationMessage = {
+      title: "Time to mine!",
+      body: `Your friend ${username} is reminding you to start your mining session`,
+    };
     const message = {
-      notification: {
-        title: "Time to mine!",
-        body: `Your friend ${username} is remininding you to start your mining session`,
-      },
+      notification: notificationMessage,
       token: fcmToken,
     };
 
     const response = await getMessaging().send(message);
     console.log("Successfully sent message:", response);
+
+    const newNotification = new Notification({senderId: senderUser._id, receiverId: receivingUser._id, message: {title: notificationMessage.title, body: notificationMessage.body}})
+    await newNotification.save();
+
     res.status(200).json({ message: "Notification sent successfully" });
   } catch (error) {
     console.error("Error sending message:", error);
