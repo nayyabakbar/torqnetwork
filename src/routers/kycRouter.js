8f1b974dc846e6725592e117ea5fd5841dbecabe
Nayyab Akbar <nayyabakbar10@gmail.com>
2024-02-22 16:11:25 +0500

    getKycStatus issue resolved

--- src/routers/kycRouter.js
+++ src/routers/kycRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const fs = require('fs');
 const path = require('path');
-
+const jwt = require("jsonwebtoken");
 
 const {personalInfo, uploadPanCardFront, uploadPanCardBack, uploadGovDocFront, uploadGovDocBack, uploadSelfie, getKycStatus} = require ("../controllers/kycController");
 
