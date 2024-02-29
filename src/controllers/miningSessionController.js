dcdf95e3ca8c8d6142f57466c2361f60bd4676a3
Nayyab Akbar <nayyabakbar10@gmail.com>
2024-02-29 21:32:31 +0500

    user getting bonus from his inviter

--- src/controllers/miningSessionController.js
+++ src/controllers/miningSessionController.js
@@ -4,29 +4,28 @@ const Staking = require("../models/stakingSchema");
 const Progress = require("../models/progressSchema");
 const schedule = require("node-schedule");
 const constants = require("../constants");
-const {sendNotificationOnProgress} = require("../../utils/notifications")
+const { sendNotificationOnProgress } = require("../../utils/notifications");
 
-async function updateRank(userId){
+async function updateRank(userId) {
   try {
-    const users = await User.aggregate( [
+    const users = await User.aggregate([
       {
-         $setWindowFields: {
-            sortBy: { availableBalance: -1 },
-            output: {
-               rank: {
-                  $denseRank: {}
-               }
-            }
-         }
-      }
-   ])
-   const user = users.find(item=> item._id.equals(userId));
-   if(user){
-    const newRank = user.rank;
-    await User.findByIdAndUpdate(userId, {$set: {rank: newRank}});  
-   }
-   return;
-
+        $setWindowFields: {
+          sortBy: { availableBalance: -1 },
+          output: {
+            rank: {
+              $denseRank: {},
+            },
+          },
+        },
+      },
+    ]);
+    const user = users.find((item) => item._id.equals(userId));
+    if (user) {
+      const newRank = user.rank;
+      await User.findByIdAndUpdate(userId, { $set: { rank: newRank } });
+    }
+    return;
   } catch (error) {
     return res.status(500).json({
       message: "An error occured",
@@ -34,29 +33,32 @@ async function updateRank(userId){
   }
 }
 
-
 async function startMining(req, res) {
   try {
-    const user = await User.findById(req.user.user) //req.user.user contains _id (from payload)
+    const user = await User.findById(req.user.user); //req.user.user contains _id (from payload)
     const currentDate = new Date();
     const lastCheckIn = user.lastCheckIn;
-    const sessions = await MiningSession.find({userId: req.user.user});
-    
-    if(sessions.length === 0){
+    const sessions = await MiningSession.find({ userId: req.user.user });
+
+    if (sessions.length === 0) {
       const newBonus = 10 * constants.baseMiningRate;
       user.availableBalance += newBonus;
       await user.save();
       const progress = await Progress.findById(user.progress);
       progress.startedEarning = true;
       await progress.save();
-      sendNotificationOnProgress(user._id, user._id, type = "earning", bonus = newBonus)
-
+      sendNotificationOnProgress(
+        user._id,
+        user._id,
+        (type = "earning"),
+        (bonus = newBonus)
+      );
     }
     const activeSession = await MiningSession.findOne({
       userId: user._id,
       isActive: true,
     });
-   
+
     if (!activeSession) {
       const newSession = new MiningSession({
         userId: user._id,
@@ -72,7 +74,7 @@ async function startMining(req, res) {
 
       //if (!lastCheckIn || timePassed <= 25 * 60 * 60 * 1000) {
 
-      if (!lastCheckIn || timePassed <= 26 * 60 * 1000) {  
+      if (!lastCheckIn || timePassed <= 26 * 60 * 1000) {
         user.streak += 1;
         if (user.streak % 6 === 0) {
           user.daysOff += 1; // Increment the user's daysOff by 1
@@ -80,7 +82,7 @@ async function startMining(req, res) {
       }
 
       //if (timePassed > 25 * 60 * 60 * 1000) {
-      
+
       if (timePassed > 26 * 60 * 1000) {
         user.streak = 0;
       }
@@ -95,7 +97,7 @@ async function startMining(req, res) {
       message: "Check-in not available!",
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).json({
       message: "An error occured!",
       error,
@@ -103,23 +105,20 @@ async function startMining(req, res) {
   }
 }
 
-
-
-function processHourlyEarnings(userId, sessionId) {
+async function processHourlyEarnings(userId, sessionId) {
   const startTime = new Date();
   // const endTime = new Date(startTime.getTime() + 23 * 60 * 60 * 1000);
 
-  const endTime = new Date(startTime.getTime() +  24 * 60 * 1000);
+  const endTime = new Date(startTime.getTime() + 23 * 60 * 1000);
   const endTime2 = new Date(endTime.getTime() + 1 * 60 * 1000);
   //const minute = startTime.getMinutes();
   //const cronJobRule = `${minute} * * * *`;
-  
+
   const cronJobRule = "* * * * *";
-  getHourlyEarnings(userId, sessionId);
+  await getHourlyEarnings(userId, sessionId);
 
   const job = schedule.scheduleJob(endTime2, async function () {
     try {
-     
       const session = await MiningSession.findByIdAndUpdate(
         sessionId,
         { $set: { isActive: false } },
@@ -137,7 +136,7 @@ function processHourlyEarnings(userId, sessionId) {
   schedule.scheduleJob(
     { start: startTime, end: endTime, rule: cronJobRule },
     async function () {
-      getHourlyEarnings(userId, sessionId);
+      await getHourlyEarnings(userId, sessionId);
     }
   );
 }
@@ -151,34 +150,57 @@ async function getHourlyEarnings(userId, sessionId) {
     userId,
     "T2"
   );
+
+  const user = await User.findById(referralId);
+  const inviterId = user.inviter;
+  const inviter = await User.findById(inviterId);
+  let inviterBonus = 0;
+  if (inviter) {
+    const activeSession = await MiningSession.findOne({
+      userId: inviter._id,
+      isActive: true,
+    });
+    if (activeSession) {
+      inviterBonus =
+        constants.tier1ReferralBonusPercentage * constants.baseMiningRate;
+    }
+  }
+
   console.log("tier 1 count", tier1Count);
   console.log("tier 2 count", tier2Count);
   console.log("tier 1 Bonus", tier1Count);
   console.log("tier 2 bonus", tier2Count);
 
   const session = await MiningSession.findById(sessionId);
-  const bonusPercentage = session.bonusWheel/100;
+  const bonusPercentage = session.bonusWheel / 100;
 
   const hourlyMiningRate =
     constants.baseMiningRate +
-    (tier1Count *(constants.tier1ReferralBonusPercentage * constants.baseMiningRate) +
-      tier2Count * (constants.tier2ReferralBonusPercentage * constants.baseMiningRate) +
-      (bonusPercentage * constants.baseMiningRate) +
-    (constants.baseMiningRate * (tier1Bonus / 100)) +
-    (constants.baseMiningRate * (tier2Bonus / 100)));
+    (tier1Count *
+      (constants.tier1ReferralBonusPercentage * constants.baseMiningRate) +
+      tier2Count *
+        (constants.tier2ReferralBonusPercentage * constants.baseMiningRate) +
+      bonusPercentage * constants.baseMiningRate +
+      constants.baseMiningRate * (tier1Bonus / 100) +
+      constants.baseMiningRate * (tier2Bonus / 100) +
+      inviterBonus);
 
-  const user = await User.findById(userId);
-  const availableBalance = user.availableBalance
-  user.availableBalance = Number((availableBalance+hourlyMiningRate).toFixed(2));
+  //const user = await User.findById(userId);
+  const availableBalance = user.availableBalance;
+  user.availableBalance = Number(
+    (availableBalance + hourlyMiningRate).toFixed(2)
+  );
   await user.save();
   const updateUserRank = await updateRank(userId);
 
   const hourlyEarnings = {
     earning: hourlyMiningRate,
     time: new Date(),
-    percentage: Number(((hourlyMiningRate / constants.baseMiningRate) * 100).toFixed(2)),
+    percentage: Number(
+      ((hourlyMiningRate / constants.baseMiningRate) * 100).toFixed(2)
+    ),
   };
-  
+
   session.activeTier1Count = tier1Count;
   session.activeTier2Count = tier2Count;
   session.hourlyEarnings.push(hourlyEarnings);
@@ -192,7 +214,6 @@ async function getActiveTiers(userId, referralType) {
     let bonus = 0;
     const referrals =
       referralType === "T1" ? user.tier1Referrals : user.tier2Referrals;
-    
 
     const promises = referrals.map(async (referralId) => {
       const referral = await User.findById(referralId);
@@ -217,70 +238,77 @@ async function getActiveTiers(userId, referralType) {
   }
 }
 
-const inactivityCheckJob = schedule.scheduleJob(' * * * * *', async function() { //All users that have been inactive since 25 hours
-  try {
-
-    
-    const inactiveUsers = await User.find({
-      //lastCheckIn: { $lt: new Date(new Date() - 25 * 60 * 60 * 1000) }
-      
-      lastCheckIn: { $lt: new Date(new Date() - 26 * 60 * 1000) }
-    });
-    for (const user of inactiveUsers) {
+const inactivityCheckJob = schedule.scheduleJob(
+  " * * * * *",
+  async function () {
+    //All users that have been inactive since 25 hours
+    try {
+      const inactiveUsers = await User.find({
+        //lastCheckIn: { $lt: new Date(new Date() - 25 * 60 * 60 * 1000) }
+        lastCheckIn: { $lt: new Date(new Date() - 26 * 60 * 1000) },
+      });
+      for (const user of inactiveUsers) {
         const daysOffAvailable = user.daysOff;
-        if (daysOffAvailable !== 0){
-            const newSession = new MiningSession({
-              userId: user._id,
-              createdAt: new Date(),
-              isActive: true,
-            });
-            await newSession.save();
-            user.streak = 0;
-            user.daysOff -= 1;
-            user.lastCheckIn = new Date();
-            user.miningSessions.push(newSession);
-            await user.save();
-            processHourlyEarnings(user._id, newSession._id);
-        }
-        else {
+        if (daysOffAvailable !== 0) {
+          const newSession = new MiningSession({
+            userId: user._id,
+            createdAt: new Date(),
+            isActive: true,
+          });
+          await newSession.save();
+          user.streak = 0;
+          user.daysOff -= 1;
+          user.lastCheckIn = new Date();
+          user.miningSessions.push(newSession);
+          await user.save();
+          processHourlyEarnings(user._id, newSession._id);
+        } else {
           const availableBalance = user.availableBalance;
-          const burningRate = (4/2400)*availableBalance;
+          const burningRate = (4 / 2400) * availableBalance;
           const newBalance = (availableBalance - burningRate).toFixed(2);
           user.availableBalance = Number(newBalance);
           await user.save();
           const updateUserRank = updateRank(user._Id);
-
         }
+      }
+    } catch (error) {
+      console.error("Error during inactivity check:", error);
     }
-  } catch (error) {
-    console.error("Error during inactivity check:", error);
   }
-});
+);
 
 inactivityCheckJob.invoke();
 
-
-schedule.scheduleJob('0 0 * * *', async function () { //All users that have been inactive since 30 days
+//schedule.scheduleJob('0 0 * * *', async function () { //All users that have been inactive since 30 days
+const longInactivity = schedule.scheduleJob("0 * * * *", async function () {
   try {
     //const users = await User.find({ lastCheckIn: { $lt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) } });
-    
-    const users = await User.find({ lastCheckIn: { $lt: new Date(Date.now() - 12 * 60 * 60 * 1000) } });
-    const promises = users.map(async(user)=>{
-    user.availableBalance = 0;
-    user.stakingBalance = 0;
-    await user.save();
-  })
-  await Promise.all(promises)
-  
+
+    const users1 = await User.find({
+      lastCheckIn: { $lt: new Date(Date.now() - 11 * 60 * 60 * 1000) },
+    });
+    const users2 = await User.find({
+      lastCheckIn: { $lt: new Date(Date.now() - 13 * 60 * 60 * 1000) },
+    });
+    const promises1 = users1.map(async (user) => {
+      user.availableBalance = 0;
+      await user.save();
+    });
+    const promises2 = users2.map(async (user) => {
+      user.stakingBalance = 0;
+      await user.save();
+    });
+    await Promise.all([...promises1, ...promises2]);
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 });
 
+longInactivity.invoke();
 
 
- // Staking Process
-async function startStaking(req, res) { 
+// Staking Process
+async function startStaking(req, res) {
   try {
     const user = await User.findById(req.user.user); //req.user.user contains _id (from payload)
     const { percentage, years } = req.body;
@@ -309,7 +337,7 @@ async function startStaking(req, res) {
         userId: user._id,
         years: years,
         amount: amount,
-        percentage: percentage
+        percentage: percentage,
       });
       await newStaking.save();
       user.stakingBalance += amount;
@@ -317,13 +345,18 @@ async function startStaking(req, res) {
       user.stakings.push(newStaking._id);
       await user.save();
 
-    const releaseDate = new Date();
-    releaseDate.setFullYear(releaseDate.getFullYear() + years);
-    const stakingJob = createStakingJob(user._id, newStaking._id, amount, years);
-    schedule.scheduleJob(releaseDate, stakingJob);
+      const releaseDate = new Date();
+      releaseDate.setFullYear(releaseDate.getFullYear() + years);
+      const stakingJob = createStakingJob(
+        user._id,
+        newStaking._id,
+        amount,
+        years
+      );
+      schedule.scheduleJob(releaseDate, stakingJob);
       return res.status(200).json({
-        message: "Staked balance successfully!"
-      })
+        message: "Staked balance successfully!",
+      });
     }
     return res.status(404).json({
       message: "Inactive users can't stake deposit",
@@ -338,13 +371,17 @@ async function startStaking(req, res) {
 function createStakingJob(userId, stakingId, amount, years) {
   return async function () {
     try {
-      const stakingDeposit = await Staking.findByIdAndUpdate(stakingId, { $set: { isActive: false } });
+      const stakingDeposit = await Staking.findByIdAndUpdate(stakingId, {
+        $set: { isActive: false },
+      });
       if (stakingDeposit) {
         const getStakedAmount = calculateInterest(amount, years);
-        const user = await User.findByIdAndUpdate(userId, { $inc: { availableBalance: getStakedAmount, stakingBalance: -amount } });
+        const user = await User.findByIdAndUpdate(userId, {
+          $inc: { availableBalance: getStakedAmount, stakingBalance: -amount },
+        });
         if (!user) {
           console.error("User not found!");
-        } 
+        }
       }
     } catch (error) {
       console.error("Error in staking job execution:", error);
@@ -352,15 +389,20 @@ function createStakingJob(userId, stakingId, amount, years) {
   };
 }
 
-
-function calculateInterest(amount,years){
-  switch(years){
-    case 1: return (0.25*amount+amount);
-    case 2: return (0.65*amount+amount);
-    case 3: return (1.20*amount+amount);
-    case 4: return (1.90*amount+amount);
-    case 5: return (2.75*amount+amount);
-    default: return 0;
+function calculateInterest(amount, years) {
+  switch (years) {
+    case 1:
+      return 0.25 * amount + amount;
+    case 2:
+      return 0.65 * amount + amount;
+    case 3:
+      return 1.2 * amount + amount;
+    case 4:
+      return 1.9 * amount + amount;
+    case 5:
+      return 2.75 * amount + amount;
+    default:
+      return 0;
   }
 }
 
